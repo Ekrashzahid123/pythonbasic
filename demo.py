c965ab1c9a7afa2908679523c7a5375c8e2315c3
Ekrash Zahid <f2022065090@umt.edu.pk>
2024-07-23 15:04:11 -0700

    learning basic string method remains

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+import random as ran
 print("Python first program")
 #Python identation --giving space in the begining of the code
 if 5>2:
@@ -51,8 +52,8 @@ print("The value of x is as",x)
 print("The value of y is as",y)
 print("The value of z is as",z)
 #----------UNPACKING CONCEPT IN PYTHON-----------------------#
-name=['Ali',"hamza","usman"]
-name_1,name_2,name_3=name
+name=['Ali',"hamza","usman"] # type: ignore
+name_1,name_2,name_3=name # type: ignore
 print("Name_1 is as",name_1)
 print("Name_2 is as",name_2)
 print("Name_3 is as",name_3)
@@ -62,7 +63,7 @@ print("Name_3 is as",name_3)
 x = "good"
 
 def myfun():
-    print("Python is " + x)
+    print("Python is " + x) # type: ignore
 
 print("Calling the function:")
 myfun()
@@ -80,4 +81,72 @@ name()
 
 print("Working as global variale beacause outside the fun")
 print("My complete name is "+name_1)
- 
+#-----------------------Data Type---------------------------------------------------------\
+#have a look on w3 school
+
+#-----------------------PYTHON NUMBER_-------------------------------------
+#Threee types 1 int 2 float 3 comples
+
+no1=1   #int
+no2=3.44 #float
+no3=2j    #complex
+
+#prinitng the typw in python for the conformation
+print("The Type of no1 is as:",type(no1))
+print("The Type of no2 is as:",type(no2))
+print("The Type of no3 is as",type(no3))
+
+#Conversion of the type to type
+
+a=float(no1)  #converting the int into float
+b=int(no2)   #converting the complex into int
+c=complex(no3) #converting the float into complex
+
+# printing the following conversion
+
+print("The conversion from int to float is as",a)
+print("The conversion from complex into int is as",b)
+print("The conversion from float into complex is as",c)
+#-------------------------MODULE RANDOM IN PYTHON USED TO GENERATE RANDOM NUMBER_-------------------------------
+print("GENERATING THE RANDOM NUMBER ")
+print("The random number is as",ran.randrange(1,9))
+#-------------------------PYTHON CASTING-------------------------------------------------------------------------
+a=1
+b=2.1
+c="5"
+print("The casting is as",int(a))
+print("The casting is as",int(b))
+print("The casting is as",int(c))
+
+#same as complex and float
+
+#------------------------PYTHON STRING----------------------------------------------------
+x="Ekrash zahid"
+print(x[:2])  #returing staring 2 element
+print(x[2:])  #returning all the element ignoring first 2
+print(x[2:5]) #output ras
+
+#--------------------Upper case string------------------------------------
+x="ahmed"
+a="USMAN"
+print("Converting all the cases to upper is as",x.upper())
+print("Converting all the cases to lower case",a.lower())
+
+#strip removes the space in end and at start
+p=" ham or ap "
+print("the output is as",p.strip())
+h='Jkrash Zahid'   #correcting the name
+print("Replace method is as",h.replace("J",'E'))
+#---------------------Formatting string----------------------------
+age=21;
+#name4="my name is ekrash and i am age old "  #this will throw an error
+name4=f"my name is ekrash and i am {age} years old "  #correct way to formatting the string
+print(name4) 
+length =2
+breath =1
+area=f"the area of square is equal to {length+breath}"
+print(area)
+
+
+
+
